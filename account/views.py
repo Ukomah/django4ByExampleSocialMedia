3d45017f329bb1e606e9ae22f383bd92b155498c
Ukomah <henryukomah@gmail.com>
2023-05-10 11:53:54 +0100

    adding account app

--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
+
 
 # Create your views here.
+
+def userLogin(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                if user.is_active:                    
+                    login(request, user)
+                    return HttpResponse('Authenticated succesfully, you are now logged in')
+                else:
+                    return HttpResponse('Your account is inactive')
+            else:
+                return HttpResponse('Invalid username or password')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'acoount.apps.AccountConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
